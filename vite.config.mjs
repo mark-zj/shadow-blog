9891fb3300c9716a6bda5bef71210974bba7d98e
mark-zj <zhou2966492419@gmail.com>
2024-12-15 10:52:21 +0800

    重写vite相关打包选项

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,7 +17,45 @@ export default defineConfig({
   mode:'development',
   publicDir:'public',
   build:{
-    outDir: "docs"
+    outDir: "docs",
+    assetsDir: './assets', // 指定静态文件引入路径为 "./assets"
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000, // 设置 chunk 大小警告的限制为 2000 KiB
+    chunkSizeLimit: 5000, // 设置 chunk 大小的限制为 5000 KiB
+    emptyOutDir: true, // 在构建之前清空输出目录
+    rollupOptions: {
+      output: {
+        // 在这里修改静态资源路径
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        // assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        assetFileNames(assetInfo) {
+          const baseUrl = 'assets'
+          if (assetInfo.name.endsWith(".css")) {
+            return `${baseUrl}/css/[name]-[hash].css`
+          }
+          if ([".png", ".jpg", ".jpeg", ".svg", ".ico", ".webp"].some((item) => assetInfo.name.endsWith(item))) {
+            return `${baseUrl}/img/[name]-[hash].[ext]`
+          }
+          if([".ttf",".eot","woff2","woff"].some((item) => assetInfo.name.endsWith(item))) {
+            return `${baseUrl}/fonts/[name]/[hash].[ext]`
+          }
+          return `${baseUrl}/[name]-[hash].[ext]`
+        },
+        manualChunks(id) {
+          // Vue 及其相关库的 chunk
+          if (id.includes('vue') || id.includes('core-js') || id.includes('@babel/runtime')) {
+            return 'vendor';
+          }
+          // 其他库的 chunk
+          if (id.includes('node_modules')) {
+            return 'dependencies';
+          }
+          // 默认情况下将模块放入一个单独的 chunk
+          return 'common';
+        },
+      },
+    },
   },
   plugins: [
     VueRouter(),
